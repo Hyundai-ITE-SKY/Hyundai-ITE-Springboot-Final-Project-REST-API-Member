5fc58636a80beff36afd8d0f930dec9ec9c47437
smc5720 <smc5720@gmail.com>
2021-11-25 15:08:28 +0900

    [SKY-43] feat: 로그인 기능 구현

--- src/main/java/com/mycompany/webapp/controller/MemberController.java
+++ src/main/java/com/mycompany/webapp/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.mycompany.webapp.controller;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,10 @@ import com.mycompany.webapp.dto.Coupon;
 import com.mycompany.webapp.dto.Member;
 import com.mycompany.webapp.dto.QnA;
 import com.mycompany.webapp.dto.WishList;
+import com.mycompany.webapp.security.JwtUtil;
 import com.mycompany.webapp.service.MemberService;
 
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -28,7 +31,7 @@ public class MemberController {
 
 //	@Resource
 //	private AuthenticationManager authenticationManager;
-	
+
 //	@RequestMapping("sendAuth")
 //	public Map<String, String> sendAuth(@RequestHeader("Authorization") String authorization) {
 //	   log.info("실행");
@@ -41,40 +44,45 @@ public class MemberController {
 //	
 	// 회원정보 보기
 	@GetMapping("/info")
-	public Member getInfo(@RequestParam String mid) {
+	public Member getInfo(HttpServletRequest request) {
 		log.info("실행");
+
+//		String jwt = request.getHeader("Authorization").substring(7);
+//		Claims claims = JwtUtil.validateToken(jwt);
+//		String mid = JwtUtil.getMid(claims);
 		
-//		Authentication auth = authenticationManager.authenticate(token);
+		String mid = request.getAttribute("mid").toString();
 		Member member = memberService.selectInfo(mid);
+
 		return member;
 	}
-	
-	//mid로 위시리스트 받아오기
+
+	// mid로 위시리스트 받아오기
 	@GetMapping("/wishlist")
 	public List<WishList> getWishList(@RequestParam String mid) {
 		List<WishList> wishList = memberService.selectWishlist(mid);
 		return wishList;
 	}
-	
-	//mid로 보유중인 쿠폰 받아오기
+
+	// mid로 보유중인 쿠폰 받아오기
 	@GetMapping("/coupon")
-	public List<Coupon> getCouponList(@RequestParam String mid){
+	public List<Coupon> getCouponList(@RequestParam String mid) {
 		List<Coupon> couponList = memberService.selectCouponlist(mid);
 		return couponList;
 	}
-	
-	//mid로 qna목록 받아오기
+
+	// mid로 qna목록 받아오기
 	@GetMapping("/qna")
-	public List<QnA> getqnaList(@RequestParam String mid){
+	public List<QnA> getqnaList(@RequestParam String mid) {
 		List<QnA> qnaList = memberService.selectQnaList(mid);
 		log.info(mid);
 		log.info(qnaList.toString());
 		return qnaList;
 	}
-	
-	//장바구니 불러오기 
+
+	// 장바구니 불러오기
 	@GetMapping("/mycart")
-	public List<Cart> getMycart(@RequestParam String mid){
+	public List<Cart> getMycart(@RequestParam String mid) {
 		List<Cart> mycart = memberService.selectMycart(mid);
 		return mycart;
 	}
